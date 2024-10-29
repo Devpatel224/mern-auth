cdb9617740b1e5fdf83944dfca88b97dd4df86a3
Dev Patel <devpatel9773@gmail.com>
2024-10-29 22:05:10 +0530

    Add signup page UI

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function SignUp() {
   return (
-    <div>SignUp</div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-8'>Sign Up</h1>
+      <form action="" className='flex flex-col gap-4 '>
+        <input className='bg-sky-100 p-3 rounded-lg' type="text" placeholder='Username' id='username'/>
+        <input className='bg-sky-100 p-3 rounded-lg' type="email" placeholder='Email' id='email'/>
+        <input className='bg-sky-100 p-3 rounded-lg' type="password" placeholder='Password' id='password'/>
+        <button className='bg-sky-700 py-3 text-white rounded-lg uppercase hover:opacity-95'>Signup</button>
+      </form>
+      <div className='flex gap-2 mt-5'>
+        <p>Have an account?</p>
+        <span className='text-blue-500'><Link to='/sign-in'>Sign in</Link></span>
+      </div>
+      </div>
   )
 }
 
