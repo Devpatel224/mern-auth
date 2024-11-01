e3f2451e33a35b34c2673b3a69b5f1dcb15419c5
Dev Patel <devpatel9773@gmail.com>
2024-11-01 11:50:17 +0530

    Add Persist to Redux Reducer

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {store} from '../src/redux/store.js'
+import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
+import { persistor } from '../src/redux/store.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
     </Provider>
-  </StrictMode>,
+  </StrictMode>, 
 )
